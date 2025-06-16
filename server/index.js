cf80d0e35abc80e96ba8cb1a429ed714b15ef575
Akin Holo <holoakinseloyin@gmail.com>
2025-06-16 09:22:24 +0100

    Renamed Server-side & client-side, structure index.js code

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const { Socket } = require("dgram");
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+   console.log("User connected: ", socket.id);
+
+   socket.on("chat message", (data) => {
+      io.emit("chat message", {
+         username: data.username,
+         message: data.message
+      });
+   });
+
+   socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+   });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+   console.log("Server running on http://localhost:${PORT}`");
+});
